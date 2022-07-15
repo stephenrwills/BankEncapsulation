376369a8cefd35da0d4e9caaf91b9bab605e9566
Stephen Wills <stephenrwills94@gmail.com>
2022-07-15 09:24:08 -0500

    finished main method

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+        
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            balance -= amount;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,21 @@
+﻿using BankEncapsulation;
+
+var myAccount = new BankAccount();
+
+Console.WriteLine($"Your balance is {myAccount.GetBalance}");
+
+Console.WriteLine("How much would you like to deposit?");
+
+var depositAmount = double.Parse(Console.ReadLine());
+
+myAccount.Deposit(depositAmount);
+
+Console.WriteLine("Please enter the amount you would like to withdraw:");
+
+var withdrawAmount = double.Parse(Console.ReadLine());
+myAccount.Withdraw(withdrawAmount);
+
+Console.WriteLine($"Thank you for your deposit! Your balance is now {myAccount.GetBalance}");
+
+
+
